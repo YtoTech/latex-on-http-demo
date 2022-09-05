abeac86245a08ecba4349c00731123c8a90ae7c4
Yoan Tournade <y@yoantournade.com>
2022-09-05 17:23:36 +0200

    Refacto code to hooks

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,5 @@
-import { h, Component } from 'preact';
+import { h,  } from 'preact';
+import { useState, useEffect } from 'preact/hooks';
 import doT from 'dot';
 import jsYaml from 'js-yaml'
 import { saveAs } from 'file-saver';
@@ -46,119 +47,154 @@ const DEFAULT_TEMPLATES = [
 		compiler: 'pdflatex',
 		latexTemplate: DEFAULT_LATEX_TEMPLATE
 	},
-	PROPOSAL_TEMPLATE,
+	// PROPOSAL_TEMPLATE,
 ]
 
+// TODO Delete or reset templates
+// TODO Compilation indicator (overlay)
 // TODO create "projects" with localstorage for each project
 
-export default class App extends Component {
+// LocalStorage.
 
-	yamlInstance = null;
-	latexInstance = null;
+const LS_MAIN_ITEM_KEY = 'templates'
+const LS_INDEX_ITEM_KEY = 'currentTemplateIndex'
 
-	state = {
-		templates: null,
-		currentTemplateIndex: null,
-
-		jsonData: null,
-		latexCompiled: null,
-		payload: null
+function loadsTemplateFromLocalStorage() {
+	let templates;
+	try {
+		templates = localStorage.getItem(LS_MAIN_ITEM_KEY);
+		console.log(templates);
+		templates = JSON.parse(templates);
+		// TODO Check them. (add version)
+	} catch(e) {
+		console.error(`Error loading templates from localStorage: ${e}`);
+	}
+	if (!templates) {
+		templates = DEFAULT_TEMPLATES;
 	}
 
-	componentWillMount() {
-		// localStorage.clear()
-		let templates = localStorage.getItem('templates')
-		if (!templates) {
-			templates = DEFAULT_TEMPLATES
-		} else {
-			console.log(templates)
-			templates = JSON.parse(templates)
-		}
+	let currentTemplateIndex;
+	try {
+		currentTemplateIndex = parseInt(localStorage.getItem(LS_INDEX_ITEM_KEY) || "0", 10)
+	} catch(e) {
+		console.error(`Error loading templates from localStorage: ${e}`);
+	}
+	if (currentTemplateIndex > templates.length-1) {
+		currentTemplateIndex = 0;
+	}
+	return [templates, currentTemplateIndex];
+}
 
-		let currentTemplateIndex = parseInt(localStorage.getItem('currentTemplateIndex') || "0", 10)
+function saveTemplatesToLocalStorage(templates) {
+	try {
+		localStorage.setItem(LS_MAIN_ITEM_KEY, JSON.stringify((templates)));
+	} catch(e) {
+		console.error(`Error saving templates to localStorage: ${e}`);
+	}
+}
 
-		this.setState({
-			templates,
-			currentTemplateIndex
-		})
+function saveTemplateIndexToLocalStorage(templateIndex) {
+	try {
+		localStorage.setItem(LS_INDEX_ITEM_KEY, templateIndex);
+	} catch(e) {
+		console.error(`Error saving templates to localStorage: ${e}`);
+	}
+}
+
+export default function LatexOnHttpDemoApp() {
+	let yamlCodeEditorInstance;
+	let latexCodeEditorInstance;
+	const [templates, setTemplates] = useState(DEFAULT_TEMPLATES);
+	const [currentTemplateIndex, setCurrentTemplateIndex] = useState(0);
+	const [jsonData, setJsonData] = useState(null);
+	const [latexCodeCompiled, setLatexCodeCompiled] = useState(null);
+	const [latexOnHttpPayload, setLatexOnHttpPayload] = useState(null);
+	useEffect(() => {
+		// Loads templates from localStorage.
+		const [
+			initialTemplates, initialCurrentTemplateIndex
+		] = loadsTemplateFromLocalStorage();
+		setTemplates(initialTemplates);
+		setCurrentTemplateIndex(initialCurrentTemplateIndex);
+		return () => {
+			// Optional: Any cleanup code
+		};
+	}, []);
+
+	console.log(templates);
+
+	const resetCompilationContext = () => {
+		setJsonData(null);
+		setLatexCodeCompiled(null);
+		setLatexOnHttpPayload(null);
 	}
 
-	onCompilerChange = e => {
-		const templates = [...this.state.templates]
-		const template = templates[this.state.currentTemplateIndex]
-		template.compiler = e.target.value
-		
-		localStorage.setItem('templates', JSON.stringify(templates))
-		this.setState({ templates })
-	}
-
-	// TODO factorize change logic
-	onNameChange = e => {
-		const templates = [...this.state.templates]
-		const template = templates[this.state.currentTemplateIndex]
-		template.name = e.target.value
-		
-		localStorage.setItem('templates', JSON.stringify(templates))
-		this.setState({ templates })
-	}
-
-	onTemplateChange = e => {
+	const onTemplateSelection = e => {
 		const idx = parseInt(e.target.value, 10)
-		if (idx === this.state.currentTemplateIndex) {
+		if (idx === currentTemplateIndex) {
 			return
 		}
 
-		if (idx === this.state.templates.length) {
-			this.setState({
-				templates: [...this.state.templates, Object.assign({}, DEFAULT_TEMPLATES[0])], 
-				currentTemplateIndex: idx
-			});
+		if (idx === templates.length) {
+			// Add new template.
+			setTemplates([...templates, Object.assign({}, DEFAULT_TEMPLATES[0])]);
+			setCurrentTemplateIndex(idx);
 		} else {
-			this.setState({ currentTemplateIndex: idx });
+			setCurrentTemplateIndex(idx);
 		}
 
-		localStorage.setItem('currentTemplateIndex', idx)
-		this.setState({ 
-			jsonData: null,
-			latexCompiled: null,
-			payload: null
-		});
+		saveTemplateIndexToLocalStorage(idx)
+		resetCompilationContext()
 	}
 
-	compile = () => {
-		if(!this.yamlInstance || !this.latexInstance) {
+	const onTemplateEntryChange = (entryKey, e) => {
+		const templatesUpdated = [...this.state.templates]
+		const templateUpdated = {
+			...templatesUpdated[currentTemplateIndex],
+			[entryKey]: e.target.value,
+		};
+		
+		saveTemplatesToLocalStorage(templates);
+		setTemplates(templates);
+	}
+
+	const compileTemplate = () => {
+		if(!yamlCodeEditorInstance || !latexCodeEditorInstance) {
 			console.error("Error with code mirrors instances. Try to reload the page.");
 			return;
 		}
 
 		// TODO display errors while parsing/templating
-
-		const templates = [...this.state.templates]
-		const template = templates[this.state.currentTemplateIndex]
+		const templatesUpdated = [...templates]
 
 		// Get inputs
-		const yamlData = this.yamlInstance.getValue()
-		const latexTemplate = this.latexInstance.getValue()
-		template.yamlData = yamlData
-		template.latexTemplate = latexTemplate
+		// TODO Really bad: use onChange to set editor values in
+		// react state. Make the editors controlled.
+		const yamlData = yamlCodeEditorInstance.getValue()
+		const latexTemplate = latexCodeEditorInstance.getValue()
+		const templateUpdated = {
+			...templatesUpdated[currentTemplateIndex],
+			yamlData,
+			latexTemplate,
+		};
 
-		localStorage.setItem('templates', JSON.stringify(templates))
-		this.setState({ templates })
+		saveTemplatesToLocalStorage(templates);
+		setTemplates(templates);
 
 		// yaml to json
 		const yamlSpaces = yamlData.replace(/\t/g, '    ')
 		console.log('yaml', yamlSpaces)
 		const jsonData = jsYaml.load(yamlSpaces);
-		this.setState({ jsonData: JSON.stringify(jsonData, null, 2) })
+		setJsonData(JSON.stringify(jsonData, null, 2));
 		console.log("yaml to json:", jsonData)
 		
 		// Compile latex template with data
 		const latexCompiled = doT.template(latexTemplate)(jsonData)
-		this.setState({ latexCompiled })
+		setLatexCodeCompiled(latexCompiled);
 		console.log("latex compiled with data:", latexCompiled)
 		
 		const payload = {
-			compiler: template.compiler,
+			compiler: templateUpdated.compiler,
 			resources: [
 				{
 					main: true,
@@ -166,14 +202,14 @@ export default class App extends Component {
 				}
 			]
 		}
-		this.setState({ payload: JSON.stringify(payload, null, 2) })
+		setLatexOnHttpPayload(JSON.stringify(payload, null, 2));
 		console.log("payload:", payload)
 
 		return payload
 	}
 
-	convertToPdf = async () => {
-		const payload = this.compile()
+	const convertToPdf = async () => {
+		const payload = compileTemplate()
 
 		const res = await fetch('https://latex.ytotech.com/builds/sync', {
 			method: 'POST',
@@ -191,117 +227,124 @@ export default class App extends Component {
 		saveAs(blob, 'default.pdf');
 	}
 
-	render() {
+	const template = templates[currentTemplateIndex]
 
-		const template = this.state.templates[this.state.currentTemplateIndex]
-
-		return (
-			<div id="app">
-				<Header />
-				<div>
-					<div class={style.input} >
-						<span>Template: </span>
-						<select value={this.state.currentTemplateIndex} onChange={this.onTemplateChange} >
-							{
-								this.state.templates.map((template, idx) => (
-									<option value={idx}>{template.name}</option>
-								))
-							}
-							<option value={this.state.templates.length}>&gt; Add new template</option>
-						</select>
-					</div>
-					<br />
-					<div class={style.input}>
-						<span>Name: </span>
-						<input type="text" value={template.name} onChange={this.onNameChange} />
-					</div>
-					<div class={style.input} >
-						<span>Compiler: </span>
-						<select value={template.compiler} onChange={this.onCompilerChange} >
-							{
-								AVAILABLE_COMPILERS.map(compiler => (
-									<option value={compiler}>{compiler}</option>
-								))
-							}
-						</select>
-					</div>
-					<button class={style.convert} onClick={this.compile}>Compile template</button>
-					<button class={style.convert} onClick={this.convertToPdf}>Convert to PDF</button>
+	return (
+		<div id="app">
+			<Header />
+			<div>
+				<div class={style.input} >
+					<span>Template: </span>
+					<select value={currentTemplateIndex} onChange={onTemplateSelection} >
+						{
+							templates.map((template, idx) => (
+								<option value={idx}>{template.name}</option>
+							))
+						}
+						<option value={templates.length}>&gt; Add new template</option>
+					</select>
 				</div>
-
-				<div style={{ clear: 'both' }} />
-
-				<div class={style.source} >
-					<h3>Source data (yaml)</h3>
-					<CodeMirror
-						code={template.yamlData}
-						config={{
-							lineNumbers: true,
-							mode: 'text/x-yaml'
-						}}
-						instance={instance => {
-							this.yamlInstance = instance;
-						}}
+				<br />
+				<div class={style.input}>
+					<span>Name: </span>
+					<input
+						type="text" value={template.name}
+						onChange={e => onTemplateEntryChange('name', e)}	
 					/>
 				</div>
-
-				<div class={style.source} >
-					<h3>Source template (LaTeX)</h3>
-					<CodeMirror
-						code={template.latexTemplate}
-						config={{
-							lineNumbers: true,
-							mode: 'text/x-stex'
-						}}
-						instance={instance => {
-							this.latexInstance = instance;
-						}}
-					/>
+				<div class={style.input} >
+					<span>Compiler: </span>
+					<select
+						value={template.compiler}
+						onChange={e => onTemplateEntryChange('compiler', e)}	
+					>
+						{
+							AVAILABLE_COMPILERS.map(compiler => (
+								<option value={compiler}>{compiler}</option>
+							))
+						}
+					</select>
 				</div>
-
-				{ this.state.jsonData &&
-					<div class={style.source} >
-						<h3>Generated data (json)</h3>
-						<CodeMirror
-							code={this.state.jsonData}
-							config={{
-								lineNumbers: true,
-								mode: 'application/json',
-								readOnly: true
-							}}
-						/>
-					</div>
-				}
-
-				{ this.state.latexCompiled &&
-					<div class={style.source} >
-						<h3>Generated LaTeX with data</h3>
-						<CodeMirror
-							code={this.state.latexCompiled}
-							config={{
-								lineNumbers: true,
-								mode: 'text/x-stex',
-								readOnly: true
-							}}
-						/>
-					</div>
-				}
-
-				{ this.state.payload &&
-					<div class={style.sourceFull} >
-						<h3>Payload for LaTeX-on-HTTP (json) </h3>
-						<CodeMirror
-							code={this.state.payload}
-							config={{
-								lineNumbers: true,
-								mode: 'application/json',
-								readOnly: true
-							}}
-						/>
-					</div>
-				}
-
+				<button class={style.convert} onClick={compileTemplate}>
+					Compile template
+				</button>
+				<button class={style.convert} onClick={convertToPdf}>
+					Convert to PDF
+				</button>
 			</div>
-		);
-	}
+
+			<div style={{ clear: 'both' }} />
+
+			<div class={style.source} >
+				<h3>Source data (yaml)</h3>
+				<CodeMirror
+					code={template.yamlData}
+					config={{
+						lineNumbers: true,
+						mode: 'text/x-yaml'
+					}}
+					instance={instance => {
+						yamlCodeEditorInstance = instance;
+					}}
+				/>
+			</div>
+
+			<div class={style.source} >
+				<h3>Source template (LaTeX)</h3>
+				<CodeMirror
+					code={template.latexTemplate}
+					config={{
+						lineNumbers: true,
+						mode: 'text/x-stex'
+					}}
+					instance={instance => {
+						latexCodeEditorInstance = instance;
+					}}
+				/>
+			</div>
+
+			{ jsonData &&
+				<div class={style.source} >
+					<h3>Generated data (json)</h3>
+					<CodeMirror
+						code={jsonData}
+						config={{
+							lineNumbers: true,
+							mode: 'application/json',
+							readOnly: true
+						}}
+					/>
+				</div>
+			}
+
+			{ latexCodeCompiled &&
+				<div class={style.source} >
+					<h3>Generated LaTeX with data</h3>
+					<CodeMirror
+						code={latexCodeCompiled}
+						config={{
+							lineNumbers: true,
+							mode: 'text/x-stex',
+							readOnly: true
+						}}
+					/>
+				</div>
+			}
+
+			{ latexOnHttpPayload &&
+				<div class={style.sourceFull} >
+					<h3>Payload for LaTeX-on-HTTP (json) </h3>
+					<CodeMirror
+						code={latexOnHttpPayload}
+						config={{
+							lineNumbers: true,
+							mode: 'application/json',
+							readOnly: true
+						}}
+					/>
+				</div>
+			}
+
+		</div>
+	);
 }
